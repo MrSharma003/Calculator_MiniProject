2c0294e905c8934831c282e270f90e592c363721
MrSharma003 <sprashant145.ms@gmail.com>
2022-04-18 16:10:36 +0530

    final commit

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -71,26 +71,23 @@ public class Calculator {
 
 
     public double factoral(double number1) {
-        logger.info("[FACTORIAL] - " + number1);
         double result = fact(number1);
-        logger.info("[RESULT - FACTORIAL] - " + result);
+        logger.info("Factorial - Input :" + number1 + " Output :" + result);
         return result;
     }
 
 
 
     public double sqroot(double number1) {
-        logger.info("[SQ ROOT] - " + number1);
         double result = Math.sqrt(number1);
-        logger.info("[RESULT - SQ ROOT] - " + result);
+        logger.info("Square root - Input : " + number1 + " Output :" + result);
         return result;
     }
 
 
     public double power(double number1, double number2) {
-        logger.info("[POWER - " + number1 + " RAISED TO] " + number2);
         double result = Math.pow(number1,number2);
-        logger.info("[RESULT - POWER] - " + result);
+        logger.info("Power - Input :" + number1 + " " + number2 + " Output :" + result);
         return result;
     }
 
@@ -109,7 +106,7 @@ public class Calculator {
         } catch (ArithmeticException error) {
             System.out.println("[EXCEPTION - LOG] - Cannot find log of negative numbers " + error.getLocalizedMessage());
         }
-        logger.info("[RESULT - NATURAL LOG] - " + result);
+        logger.info("Natural Log - Input :" + number1 + " Output :" + result);
         return result;
     }
     public double fact(double num) {
